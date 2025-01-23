c445ddadbf4159672e51f9be752b69cdbd9748c1
AccessiT3ch <accessiT3ch@gmail.com>
2025-01-22 17:15:32 -0800

    Set vite production url basename for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        quietDeps: true,
-        additionalData: `@import "./src/scss/variables.scss";`,
+export default defineConfig(() => {
+  const env = process?.env || {};
+  return {
+    plugins: [react()],
+
+    css: {
+      preprocessorOptions: {
+        scss: {
+          quietDeps: true,
+          additionalData: `@import "./src/scss/variables.scss";`,
+        },
       },
     },
-  },
+
+    // base url should be /ReadCards/ for production and / for development
+    base: env.VITE_APP_ENV === "production" ? "/ReadCards/" : "/",
+  };
 });
