3ec03b23dfe62c539a8f5d8c51553174c98e40ae
AccessiT3ch <accessiT3ch@gmail.com>
2025-01-27 13:32:46 -0800

    Move content variables into content module

--- src/Components/Resources.jsx
+++ src/Components/Resources.jsx
@@ -1,41 +1,7 @@
 import { Col, Dropdown, Row } from "react-bootstrap";
 import { titleCase } from "../utils";
+import { digitalResources, redCardsPrintLinks } from "./content";
 
-const redCardsPrintLinks = {
-  arabic:
-    "https://www.ilrc.org/sites/default/files/documents/red_card-self_srv-arabic.pdf",
-  chinese:
-    "https://www.ilrc.org/sites/default/files/resources/red_card-self_srv-chinese.pdf",
-  english:
-    "https://www.ilrc.org/sites/default/files/documents/red_card-self_srv-english.pdf",
-  farsi:
-    "https://www.ilrc.org/sites/default/files/2024-12/Artwork%20for%20Printing%20Your%20Own%20Red%20Cards%20-%20Farsi.pdf",
-  french:
-    "https://www.ilrc.org/sites/default/files/documents/red_card-self_srv-french-20190603.pdf",
-  haitian_creole:
-    "https://www.ilrc.org/sites/default/files/2023-06/Artwork%20for%20Printing%20Your%20Own%20Red%20Cards%20-%20Haitian%20Creole.pdf",
-  hmong:
-    "https://www.ilrc.org/sites/default/files/documents/red_card-self_srv-hmong-20190917.pdf",
-  korean:
-    "https://www.ilrc.org/sites/default/files/documents/red_card-self_srv-korean.pdf",
-  pashto:
-    "https://www.ilrc.org/sites/default/files/2024-12/Artwork%20for%20Printing%20Your%20Own%20Red%20Cards%20-%20Pashto.pdf",
-  punjabi:
-    "https://www.ilrc.org/sites/default/files/documents/artwork_for_printing_your_own_red_cards_-_punjabi.pdf",
-  russian:
-    "https://www.ilrc.org/sites/default/files/documents/artwork_for_printing_your_own_red_cards_-_russian.pdf",
-  spanish:
-    "https://www.ilrc.org/sites/default/files/2025-01/Artwork%20for%20Printing%20Your%20Own%20Red%20Cards%20-%20Spanish.pdf",
-  tagalog:
-    "https://www.ilrc.org/sites/default/files/documents/red_card-self_srv-tagalog-20190603.pdf",
-  tigrinya:
-    "https://www.ilrc.org/sites/default/files/2024-12/Artwork%20for%20Printing%20Your%20Own%20Red%20Cards%20-%20Tigrinya.pdf",
-
-  ukrainian:
-    "https://www.ilrc.org/sites/default/files/documents/artwork_for_printing_your_own_red_cards_-_ukrainian.pdf",
-  vietnamese:
-    "https://www.ilrc.org/sites/default/files/documents/red_card-self_srv-vietnamese-20190603.pdf",
-};
 
 function Resources() {
   return (
