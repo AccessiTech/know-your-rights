18c431ccf0c0f6355ff346f506b69d9f160c9a41
AccessiT3ch <accessiT3ch@gmail.com>
2025-01-29 14:00:10 -0800

    Add rights CTA data for resource button

--- src/Components/Rights/content.jsx
+++ src/Components/Rights/content.jsx
@@ -1,3 +1,4 @@
+
 // content for the right side of the Rights section
 export const rightHeader = "To the agent";
 export const rightColContent = (
@@ -66,4 +67,139 @@ export const attribution = (
 // Call to Action content for the bottom of the Rights section
 export const ctaUrl =
   "https://www.informedimmigrant.com/resources/detention-deportation/know-your-rights/";
-export const ctaTitle = "More about YOUR RIGHTS";
+export const ctaTitle = "CA Rapid Response Networks";
+export const ctaSource = "caimmigrant.org";
+
+export const ctaData = {
+  url: ctaUrl,
+  title: ctaTitle,
+  description: "See something, say something.",
+  links: [
+    {
+      title: "North California",
+      description:
+        "Central del Pueblo Rapid Response Network (Humboldt County) ",
+      descriptionLink: "https://www.instagram.com/centrodelpueblo",
+      url: "tel:707-200-8091",
+      btnText: "707-200-8091",
+    },
+    {
+      description: "Sacramento Rapid Response Network",
+      descriptionLink:
+        "https://www.sacfuelnetwork.org/sacramento-rapid-response-network",
+      url: "tel:916-382-0256",
+      btnText: "916-382-0256",
+    },
+    {
+      title: "Bay Area",
+      description:
+        "​Alameda County Immigration Legal and Education Partnership (ACILEP)",
+      descriptionLink: "http://centrolegal.org/acilep/",
+      url: "tel:510-241-4011",
+      btnText: "510-241-4011",
+    },
+    {
+      description: "Marin Rapid Response Network",
+      descriptionLink: "https://www.marinrrn.org/",
+      url: "tel:415-991-4545",
+      btnText: "415-991-4545",
+    },
+    {
+      description:
+        "North Bay Rapid Response Network (Sonoma, Napa & Solano Counties)",
+      descriptionLink: "https://www.northbayop.org/nbrrn",
+      url: "tel:707-800-4544",
+      btnText: "707-800-4544",
+    },
+    {
+      description: "San Francisco Rapid Response Network",
+      descriptionLink: "http://sfilen.org/",
+      url: "tel:415-200-1548",
+      btnText: "415-200-1548",
+    },
+    {
+      description: "San Mateo County Rapid Response Network",
+      descriptionLink: "https://faithinactionba.org/",
+      url: "tel:203-666-4472",
+      btnText: "203-666-4472",
+    },
+    {
+      description: "Santa Clara County Rapid Response Network",
+      descriptionLink:
+        "https://pactsj.org/rapid-response-network-in-santa-clara-county",
+      url: "tel:408-290-1144",
+      btnText: "408-290-1144",
+    },
+    {
+      description: "Stand Together Contra Costa",
+      descriptionLink: "https://standtogethercontracosta.org/",
+      url: "tel:925-900-5151",
+      btnText: "925-900-5151",
+    },
+    {
+      title: "Central Coast & Central Valley",
+      description: "Kern County Rapid Response Network",
+      descriptionLink: "https://www.rapidresponsekern.com/",
+      url: "tel:661-432-2230",
+      btnText: "661-432-2230",
+    },
+    {
+      description:
+        "Santa Cruz County Your Allied Rapid Response Network & Pajaro Valley Rapid Response",
+      descriptionLink: "https://www.facebook.com/YARRsantacruz/",
+      url: "tel:831-239-4289",
+      btnText: "831-239-4289",
+    },
+    {
+      description:
+        "Southern Central Coast Rapid Response Network (Santa Barbara, Ventura & San Luis Obispo Counties)",
+      descriptionLink: "http://www.805immigrant.org/",
+      url: "tel:805-870-8855",
+      btnText: "805-870-8855",
+    },
+    {
+      description: "Valley Watch Rapid Response Network",
+      url: "tel:559-206-0151",
+      btnText: "559-206-0151",
+    },
+    {
+      title: "Southern California",
+      description: "Long Beach Community Defense Network",
+      descriptionLink: "https://www.sanctuarylb.com/lbcdn",
+      url: "tel:562-276-0267",
+      btnText: "562-276-0267",
+    },
+    {
+      description: "Los Angeles Raids and Rapid Response Network",
+      url: "tel:888-624-4752",
+      btnText: "888-624-4752",
+    },
+    {
+      description:
+        "Orange County Rapid Response Network (casereferral@ocrapidresponse.org)",
+      descriptionLink: "https://ocrapidresponse.org/",
+      url: "tel:714-881-1558",
+      btnText: "714-881-1558",
+    },
+    {
+      description:
+        "(San Bernardino & Riverside Counties)​ Inland Coalition For Immigrant Justice’s Resource",
+      descriptionLink: "http://ic4ij.org/",
+      url: "tel:909-361-4588",
+      btnText: "909-361-4588",
+    },
+    {
+      description:
+        "Southern California Bilingual Rapid Response Legal Resource Hotline",
+      descriptionLink: "https://www.immdef.org/blog/immdefrrhotline",
+      url: "tel:213-833-8283",
+      btnText: "213-833-8283",
+    },
+    {
+      description: "San Diego Rapid Response Network",
+      descriptionLink: "http://www.rapidresponsesd.org/",
+      url: "tel:619-536-0823",
+      btnText: "619-536-0823",
+    },
+  ],
+};
